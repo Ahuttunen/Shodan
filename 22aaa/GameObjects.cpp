839748c5cf40339141a6635942896445b21440f1
Asko Huttunen <asko.huttunen@outlook.com>
2015-02-12 21:07:47 +0200

    Update GameObjects.cpp

--- 22aaa/GameObjects.cpp
+++ 22aaa/GameObjects.cpp
@@ -15,5 +15,11 @@ void GameObjects::update(sf::Time deltaTime)
 }
 void GameObjects::draw(sf::RenderWindow& myWindow)
 {
+	//Vaatii muokkausta sitten ku on pelaajalla grafiikka
 	myWindow.draw(shape);
+}
+sf::Vector2f GameObjects::getPosition()
+{
+	//Positionin saanti
+	return shape.getPosition();
 }
