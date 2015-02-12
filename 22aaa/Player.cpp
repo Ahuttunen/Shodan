5a6459c39552557393c9fc6af082d75b904a8236
Asko Huttunen <asko.huttunen@outlook.com>
2015-02-12 21:10:34 +0200

    Update Player.cpp

--- 22aaa/Player.cpp
+++ 22aaa/Player.cpp
@@ -13,48 +13,58 @@ Player::~Player(void)
 {
 
 }
-void Player::update(sf::Time deltatime)
+void Player::PlayerInputs(sf::Keyboard::Key key, bool Pressed)
 {
-	int PlayerSpeed = 150;
-	if (Up)
-	{
 
-	}
-	if (Down)
-	{
-
-	}
-	if (Left)
-	{
-
-	}
-	if (Right)
-	{
-
-	}
-}
-void Player::PlayerInputs(sf::Keyboard::Key key, bool isPressed)
-{
+	//Liikkumista varten checkaa että jos painaa esim ylös(up), niin se antaa sille arvon
+	//Pressed jolloin se liikuttaa pelajaa siihen suuntaan seuraavassa funktiossa
 	if (key == sf::Keyboard::Up)
 	{
-
+		Up = Pressed;
 	}
 	if (key == sf::Keyboard::Down)
 	{
-		
+		Down = Pressed;
 	}
 	if (key == sf::Keyboard::Left)
 	{
-		
+		Left = Pressed;
 	}
 	if (key == sf::Keyboard::Right)
 	{
-		
+		Right = Pressed;
 	}
 }
+void Player::update(sf::Time deltatime)
+{
+//Liikkuminen vaatii vielä että checkais että ei mee yli "kuvan" y- ja x-akselilla
+	int PlayerSpeed = 150;
+	sf::Vector2f move(0, 0);
+	//T
+	if (Up)
+	{
+		move.y += PlayerSpeed;
+	}
+	if (Down)
+	{
+		move.y -= PlayerSpeed;
+	}
+	if (Left)
+	{
+		move.x += PlayerSpeed;
+	}
+	if (Right)
+	{
+		move.x -= PlayerSpeed;
+	}
+	shape.move(move*deltatime.asSeconds());
+}
+
 
 void Player::draw(sf::RenderWindow& myWindow)
 {
+	//Tällä hetkellä vaan piirtää pallon. Vaatii muokkausta kun ollaan saatu.
+	//Pelaajalle grafiikka.
 	sf::CircleShape shape(100.f);
 	shape.setFillColor(sf::Color::Green);
 	myWindow.draw(shape);
